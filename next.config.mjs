e4faefc35ff336960c02a13cda6728de2cbf1f12
Overseer1 <lizaso.157383@stamaria.sti.edu.ph>
2024-06-09 19:46:54 +0800

    commit (credits kay bais)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { 
+    basePath: "/LasCartas",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
